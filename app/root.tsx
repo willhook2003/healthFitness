6fae95491bb9928357cd63aa845197341907c2c4
Willy Ramirez <willhook2003@gmail.com>
2025-07-23 00:38:13 -0300

    integrate TanStack Query and Table libraries with global client setup
    - Added @tanstack/react-query and @tanstack/react-table to project
    - Created shared QueryClient instance in app/lib/queryClient.ts
    - Wrapped root layout with <QueryClientProvider>

--- app/root.tsx
+++ app/root.tsx
@@ -6,6 +6,8 @@ import {
   Scripts,
   ScrollRestoration,
 } from "react-router";
+import { QueryClientProvider } from "@tanstack/react-query";
+import { queryClient } from "./lib//queryClient";
 
 import type { Route } from "./+types/root";
 import "./app.css";
@@ -42,7 +44,11 @@ export function Layout({ children }: { children: React.ReactNode }) {
 }
 
 export default function App() {
-  return <Outlet />;
+  return (
+    <QueryClientProvider client={queryClient}>
+      <Outlet />
+    </QueryClientProvider>
+  );
 }
 
 export function ErrorBoundary({ error }: Route.ErrorBoundaryProps) {
